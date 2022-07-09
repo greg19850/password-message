72bd7ca3fce3a1414ba57dce5cc091893920c443
greg@gregPredator <greg19850@gmail.com>
2022-07-09 11:16:20 +0100

    created password & message variables. Created showMessage function

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 const input = document.querySelector('input');
 const divMessage = document.querySelector('.message');
+
+let password = 'user';
+let message = 'Example message to show';
+
+const showMessage = function (e) {
+
+    if (e.target.value == password) {
+        divMessage.textContent = message
+    } else { divMessage.textContent = '' }
+}
+
+
+const focusChange = function () {
+    input.style.backgroundColor = 'black';
+    input.style.color = 'white'
+}
+
+input.addEventListener('input', showMessage);
+input.addEventListener('focus', focusChange);
